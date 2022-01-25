8b00807d0d9cbfa419fdfc028162bffe4bf4eb51
Chynna Lew <chynnalew@yahoo.com>
2022-01-25 13:31:48 -0800

    store player name input in game state

--- components/CharacterCreate.js
+++ components/CharacterCreate.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, Button} from 'react-native';
+import {Keyboard, SafeAreaView, StyleSheet, Text, Button, TextInput} from 'react-native';
 import {Actions} from 'react-native-router-flux';
 
-const CharacterCreate = () => {
+const CharacterCreate = (props) => {
     const goBack = () => {
         Actions.menu()
     }
 
     return(
         <SafeAreaView style={styles.container}>
-            <Text style={styles.text}>CharacterCreation page</Text>
+            <Text style={[styles.text, styles.title]}>Create Your Character</Text>
+            <Text style={[styles.text, styles.inputText]}>Name</Text>
+            <TextInput
+                style={styles.input}
+                maxLength={50}
+                onChangeText={props.onChangeName}
+                defaultValue={props.playerName}
+            />
             <Button style={[styles.text, styles.button]} title='Go Back' onPress={goBack}/>
         </SafeAreaView>
     )
@@ -18,10 +25,24 @@ const CharacterCreate = () => {
 const styles = StyleSheet.create({
     container:{
         flex: 1,
+        alignItems: 'center',
         backgroundColor:'#000',
+    },
+    input:{
+        backgroundColor:'#fff',
+        margin: 5,
+        padding: 5,
+        width: '50%'
+    },
+    inputText:{
+        fontSize: 20,
     },
     text:{
         color:'#fff'
+    },
+    title:{
+        fontSize: 30,
+        marginBottom: 10,
     }
 })
 
--- components/Game.js
+++ components/Game.js
@@ -2,9 +2,26 @@ import React from 'react';
 import CharacterCreate from './CharacterCreate';
 
 class Game extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            playerName: 'test name',
+            health: 100,
+            day: 1,
+            items: [],
+        }
+    }
+
+    handleChangeName = (text) => {
+        this.setState({
+            playerName: text
+        })
+    }
     render() {
         return(
-            <CharacterCreate/>
+            <CharacterCreate 
+                playerName={this.state.playerName}
+                onChangeName = {this.handleChangeName}/>
         )
     }
 }
